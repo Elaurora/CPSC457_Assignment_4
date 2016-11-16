b65ce593b93ecd17034c553b27aa2ea28847fb86
Elaurora <joshua.walters@ucalgary.ca>
2016-11-16 08:54:50 -0700

    Just finished wednesday tutorial

--- src/WriteBuffer.java
+++ src/WriteBuffer.java
@@ -11,17 +11,44 @@ public class WriteBuffer {
 	 * If true buffer uses TSO, FIFO
 	 * If false buffer uses PSO, and it is FIFO per variable
 	 */
-	boolean TSO;
+	private boolean tso;
+	
+	/**
+	 * Creates a WriteBuffer with the indicated write 
+	 */
+	public WriteBuffer(boolean writeAlgorithm){
+		
+	}
 	
 	
-	public PendingStore NextValueToBeStored(){
-		
-		
-		
-		
+	/**
+	 * 
+	 * @return The next value that is set to to be sent to main main memory. Returns null if the buffer is empty
+	 */
+	public PendingStore nextValueToBeStored(){
+		//TODO:this
 		
 		return null;
 	}
 	
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public Integer load(String index){
+		//TODO:this
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @param value
+	 */
+	public void store(String index, Integer value){
+		//TODO:this
+	}
 	
 }
