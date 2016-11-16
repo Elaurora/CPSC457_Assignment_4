0683c1153935e03cc8aa0a36ef5a7a4e975f9fd2
Elaurora <joshua.walters@ucalgary.ca>
2016-11-16 08:41:50 -0700

    Added method to WriteBuffer

---
+++ src/NotInBufferException.java
@@ -0,0 +1,11 @@
+
+
+
+/**
+ * Used by the WriteBuffer Method when load is called on
+ * @author joshua
+ *
+ */
+public class NotInBufferException extends Exception{
+
+}
--- src/WriteBuffer.java
+++ src/WriteBuffer.java
@@ -1,4 +1,27 @@
 
-public class WriteBuffer {
 
+/**
+ * 
+ * @author joshua
+ *
+ */
+public class WriteBuffer {
+	
+	/**
+	 * If true buffer uses TSO, FIFO
+	 * If false buffer uses PSO, and it is FIFO per variable
+	 */
+	boolean TSO;
+	
+	
+	public PendingStore NextValueToBeStored(){
+		
+		
+		
+		
+		
+		return null;
+	}
+	
+	
 }
